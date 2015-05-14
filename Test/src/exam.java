abeeda74055f53046fe70642420308606e0c35b7
stella <stella@stella-pc>
2015-05-14 16:19:44 +0900

    커밋앤푸시

---
+++ Test/src/exam.java
@@ -0,0 +1,21 @@
+/*역순으로 출력*/
+import java.util.Scanner;
+
+public class exam {
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+int intVar;
+		
+//		//for문 사용
+//		System.out.print("입력값: ");
+//		String input = sc.nextLine();
+//		for(int i=input.length()-1;i>=0;i--){
+//			System.out.print("출력값: "+input.charAt(i));
+//		}
+//		
+//		//StringBuffer 사용
+//		System.out.print("입력값: ");
+//		StringBuffer sb = new StringBuffer(sc.nextLine());
+//		System.out.print("출력값: "+sb.reverse());
+	}
+}
