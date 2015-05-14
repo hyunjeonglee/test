de11ecfb66fc9262cf3fea15008f06a0a2fb43a7
stella <stella@stella-pc>
2015-05-14 16:17:25 +0900

    커밋되나용

---
+++ Test/src/Switch.java
@@ -0,0 +1,21 @@
+
+public class Switch {
+
+	public static void main(String[] args) {
+		int a=10;
+		switch(a){
+			case 10 :
+				System.out.println("10점");
+			case 20 :
+				System.out.println("20점"); break;
+			case 30 :
+				System.out.println("30점");
+			case 40 :
+				System.out.println("40점");
+			default :
+				System.out.println("0점");
+			
+		}
+	}
+
+}
